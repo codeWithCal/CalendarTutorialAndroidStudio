5ee3941d8fc4f4f2bb971731abffd33efdcecc82
Callum Hill <ccahill.is@gmail.com>
2021-05-30 15:33:29 +1000

    Weekly Calendar

---
+++ app/src/main/java/codewithcal/au/calendarappexample/EventAdapter.java
@@ -0,0 +1,37 @@
+package codewithcal.au.calendarappexample;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.List;
+
+public class EventAdapter extends ArrayAdapter<Event>
+{
+    public EventAdapter(@NonNull Context context, List<Event> events)
+    {
+        super(context, 0, events);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent)
+    {
+        Event event = getItem(position);
+
+        if (convertView == null)
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.event_cell, parent, false);
+
+        TextView eventCellTV = convertView.findViewById(R.id.eventCellTV);
+
+        String eventTitle = event.getName() +" "+ CalendarUtils.formattedTime(event.getTime());
+        eventCellTV.setText(eventTitle);
+        return convertView;
+    }
+}
---
+++ app/src/main/java/codewithcal/au/calendarappexample/EventEditActivity.java
@@ -0,0 +1,44 @@
+package codewithcal.au.calendarappexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.time.LocalTime;
+
+public class EventEditActivity extends AppCompatActivity
+{
+    private EditText eventNameET;
+    private TextView eventDateTV, eventTimeTV;
+
+    private LocalTime time;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_edit);
+        initWidgets();
+        time = LocalTime.now();
+        eventDateTV.setText("Date: " + CalendarUtils.formattedDate(CalendarUtils.selectedDate));
+        eventTimeTV.setText("Time: " + CalendarUtils.formattedTime(time));
+    }
+
+    private void initWidgets()
+    {
+        eventNameET = findViewById(R.id.eventNameET);
+        eventDateTV = findViewById(R.id.eventDateTV);
+        eventTimeTV = findViewById(R.id.eventTimeTV);
+    }
+
+    public void saveEventAction(View view)
+    {
+        String eventName = eventNameET.getText().toString();
+        Event newEvent = new Event(eventName, CalendarUtils.selectedDate, time);
+        Event.eventsList.add(newEvent);
+        finish();
+    }
+}
